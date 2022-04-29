5744bd7579c1cbe8b3f2c897a3a39feb384b98ff
Anusiyaram <annathai.r@gmail.com>
2022-04-29 11:32:07 +0530

    config file change

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,9 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: `https://strapi-gatsby-postgresql-pro.herokuapp.com/api/`,
+        apiURL: process.env.DEPLOY_URL
+          ? "https://strapi-gatsby-postgresql-pro.herokuapp.com"
+          : "http://localhost:1337",
         collectionTypes: ["article", "category"],
         queryLimit: 1000,
       },
